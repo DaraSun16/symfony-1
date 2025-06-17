03e897b25f06e0c71762c214594b49309725a176
dara <dara.tamako@gmail.com>
2025-06-17 11:26:08 +0200

    controller/base/menu

--- src/Api/MenuController.php
+++ src/Api/MenuController.php
@@ -1,13 +1,17 @@
 <?php
-namespace App\Controller\Api;
+namespace App\Api;
 
+use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class MenuController extends AbstractController
 {
-  public function navbar(): Response{
-    return $this->render('_partials/menu.html.twig');
+  public function __construct(private CategoriesRepository $categoriesRepository){}
+
+  public function navbar(): Response
+  {
+    return $this->render('_partials/menu.html.twig', ['categories'=>$this->categoriesRepository->findAll()]);
   }
 }
 
