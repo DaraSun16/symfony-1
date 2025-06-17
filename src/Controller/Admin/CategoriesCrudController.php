46217cd088aef6c12cd3889956a4051f86a1a776
dara <dara.tamako@gmail.com>
2025-06-17 09:55:04 +0200

    categories/mime/crud

--- src/Controller/Admin/CategoriesCrudController.php
+++ src/Controller/Admin/CategoriesCrudController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Categories;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class CategoriesCrudController extends AbstractCrudController
 {
@@ -15,14 +19,19 @@ class CategoriesCrudController extends AbstractCrudController
         return Categories::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            ImageField::new('imageName')
+                ->setBasePath('/image/categorie')
+                ->onlyOnIndex(),
+            TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            Field::new('imageFile')
+                ->setFormType(VichImageType::class)
+                ->onlyOnForms()
         ];
     }
-    */
 }
